1f746b1afce5252d3a5b04548a6112d1132d4315
Boshra Hashemi <boshra@backbase.com>
2025-05-06 22:55:13 +0200

    adding spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,23 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.spotless)
+}
+spotless {
+    kotlin {
+        target("**/*.kt", "**/*.kts")
+        targetExclude("$buildDir/**/*.kt", "bin/**/*.kt", "buildSrc/**/*.kt")
+        // version, editorConfigPath, editorConfigOverride and customRuleSets are all optional
+        ktlint().setEditorConfigPath("$rootDir/.editorconfig") // sample unusual placement
+    }
+}
+val installGitHook by tasks.registering(Copy::class) {
+    from(file("${rootProject.rootDir}/.scripts/pre-commit"))
+    into(file("${rootProject.rootDir}/.git/hooks"))
+    fileMode = 0b111101101
+}
+project(":composeApp").afterEvaluate {
+    tasks.named("preBuild").configure {
+        dependsOn(":installGitHook")
+    }
 }
